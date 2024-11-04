2018c7945e36f2e2bc0524ad8f5eff9e07be5ea8
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-11-04 08:24:21 +0200

    updates

--- controllers/messages.js
+++ controllers/messages.js
@@ -7,53 +7,31 @@ const Conversation = require('../models/conversation');
 exports.sendMessage = async (req, res) => {
   try {
     const { conversationId, senderId, content } = req.body;
-    
-    // Create new message
-    const newMessage = await messageModel.create({
-      conversationId,
-      senderId,
-      content,
-      readBy: [senderId]
-    });
+    const newMessage = { conversationId, senderId, content };
 
-    // Populate sender details
-    const populatedMessage = await messageModel.findById(newMessage._id)
+    const savedMessage = await messageModel.create(newMessage);
+    
+    // Populate sender information
+    const populatedMessage = await messageModel.findById(savedMessage._id)
       .populate('senderId', 'firstName lastName profilePicture');
 
     // Update conversation's last message
     const conversation = await Conversation.findByIdAndUpdate(
       conversationId,
-      { lastMessage: newMessage._id },
+      { lastMessage: savedMessage._id },
       { new: true }
     ).populate('projectId client freelancerId');
 
-    // Format message for Pusher
-    const messageForPusher = {
-      _id: populatedMessage._id,
-      content: populatedMessage.content,
-      senderId: populatedMessage.senderId._id, // Just send the ID
-      createdAt: populatedMessage.createdAt,
-      readBy: populatedMessage.readBy
-    };
-
     const pusher = req.app.get('pusher');
     
-    // Trigger new message event
-    pusher.trigger(
-      `conversation-${conversationId}`,
-      'new-message',
-      messageForPusher
-    );
+    // Trigger new message event in conversation channel
+    pusher.trigger(`conversation-${conversationId}`, 'new-message', populatedMessage);
 
-    // Send notification to recipient
+    // Determine recipient and send notification
     const recipientId = conversation.client._id.toString() === senderId 
       ? conversation.freelancerId._id.toString()
       : conversation.client._id.toString();
 
-    pusher.trigger(`user-${recipientId}`, 'message-notification', {
-      conversationId: conversation._id,
-      message: messageForPusher
-    });
     // Trigger notification in recipient's channel
     pusher.trigger(`user-${recipientId}`, 'message-notification', {
       _id: savedMessage._id,
