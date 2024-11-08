27a7a54d2bd5a39317aa69a7c2b827a05d845593
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-11-08 13:20:28 +0200

    updates

--- routes/platformEarning.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/platfoemEarning');
-// const { protect, adminOnly } = require('../middleware/authMiddleware');
-
-router.get('/earnings', adminController.getPlatformEarnings);
-router.get('/earnings/report',  adminController.getEarningsReport);
-
-module.exports = router;
